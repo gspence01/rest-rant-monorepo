1ca040675f0d2ae4c7a848de357e36b5a883a007
Gabrielle <gabrielle01.spence@gmail.com>
2023-04-16 16:51:35 -0700

    created sessions

--- frontend/src/users/LoginForm.js
+++ frontend/src/users/LoginForm.js
@@ -17,8 +17,9 @@ function LoginForm() {
 
     async function handleSubmit(e) {
         e.preventDefault()
-        const response = await fetch(`http://localhost:5000/authentication/`, {
+        const response = await fetch('http://localhost:5000/authentication/', {
             method: 'POST',
+            credentials: 'include',
             headers: {
                 'Content-Type': 'application/json'
             },
